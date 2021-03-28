3b248c3d7f12a64e3adaf7645b946fe9092e5653
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-03-28 20:33:34 +0200

    fix(material-icons): move link element to head

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import StoreProvider from './providers/StoreProvider';
 function App() {
   return (
     <StoreProvider>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
       <div className="app page">
         <div className="main-content">
           <Search></Search>
