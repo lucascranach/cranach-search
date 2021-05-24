d28f1f030cd9daabee6605ff19b30fd2891cb9a6
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-05-24 13:01:21 +0200

    chore(artefact-overview): remove unused imports

--- src/components/structure/visualizing/artefact-overview/artefact-overview.tsx
+++ src/components/structure/visualizing/artefact-overview/artefact-overview.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from 'react';
+import React, { FC } from 'react';
 
 import Switcher from '../../../base/interacting/switcher';
 import ArtefactCard from '../artefact-card';
