498e753c44a05c0a5b0a9ca7d5beb11532251e2b
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2021-11-13 00:49:02 +0100

    feat(lang): reintroduce simple language routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
+import StoreContext from './store/StoreContext';
 import Dashboard from './components/pages/dashboard';
 import Navigation from './components/structure/interacting/navigation';
 
 
 function App() {
+  const { root: { routing, ui } } = useContext(StoreContext);
+
+  useEffect(() => {
+    const match = routing.history.location.pathname.match(/^\/([a-z]+)\//);
+    if(match && ui.allowedLangs.includes(match[1])) {
+      ui.setLanguage(match[1]);
+    } else {
+      routing.history.replace({ ...routing.history.location, pathname: `${import.meta.env.BASE_URL}${ui.lang}/` });
+    }
+  }, [ui.allowedLangs, routing.history]);
+
+  useEffect(() => {
+    routing.history.replace({ ...routing.history.location, pathname: `${import.meta.env.BASE_URL}${ui.lang}/` });
+  }, [ui.lang, routing.history]);
+
   return (
     <Fragment>
       <Navigation></Navigation>
