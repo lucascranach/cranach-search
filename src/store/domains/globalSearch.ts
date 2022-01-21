dcf1fc4ac3a8ec33feaf2d59932f3bda7e0fdc50
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2022-01-21 16:20:17 +0100

    fix(globalSearch): check correctly for entityType param value

--- src/store/domains/globalSearch.ts
+++ src/store/domains/globalSearch.ts
@@ -358,7 +358,7 @@ export default class GlobalSearch implements GlobalSearchStoreInterface, Routing
   }
 
   private handleRoutingNotificationForEntityType(value: string) {
-    if (value in Object.keys(EntityType)){
+    if (Object.values(EntityType).includes(value as EntityType)){
       this.filters.entityType = value as EntityType;
     }
   }
