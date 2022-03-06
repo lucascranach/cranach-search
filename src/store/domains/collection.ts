6a6747142119e84be2905d6a58557ea79584cec4
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2022-03-06 15:48:54 +0100

    refactor(collection): allow storage of structured artefact infos in collection

--- src/store/domains/collection.ts
+++ src/store/domains/collection.ts
@@ -1,12 +1,22 @@
 
 import { makeAutoObservable } from 'mobx';
+import { EntityType } from '../../api/globalSearch';
 import { RootStoreInterface } from '../rootStore';
 
 const cranachCompareURL = import.meta.env.VITE_CRANACH_COMPARE_URL;
 
+const CRANACH_SEARCH_LOCALSTORAGE_KEY = 'collection';
+
+interface CollectionItem {
+  inventoryNumber: string;
+  objectName: string;
+  entityType: EntityType,
+  collectedAtTimestamp: string;
+};
+
 export default class Collection implements CollectionStoreInterface {
   rootStore: RootStoreInterface;
-  artefacts: string[] = [];
+  artefacts: CollectionItem[] = [];
 
   constructor(rootStore: RootStoreInterface) {
     makeAutoObservable(this);
@@ -19,52 +29,82 @@ export default class Collection implements CollectionStoreInterface {
     return this.artefacts.length;
   }
 
-  readFromLocalStorage(): void {
-    throw new Error('Method not implemented.');
-  }
-
   /* Actions */
-  addArtefactToCollection(artefact: string) {
-    this.artefacts.push(artefact);
-    localStorage.setItem('collection', this.artefacts.join(','));
+  addArtefactToCollection(inventoryNumber: string) {
+    const matchingItem = this.rootStore.globalSearch.result?.items.find(item => item.id === inventoryNumber);
+
+    if (!matchingItem) return;
+
+    const mappedItem: CollectionItem = {
+      inventoryNumber: matchingItem.id,
+      objectName: matchingItem.objectName,
+      entityType: matchingItem.entityType,
+      collectedAtTimestamp: (new Date()).toISOString(),
+    };
+
+    this.artefacts.push(mappedItem);
+    this.storeCollectionToLocalStorage();
+
     return true;
   }
 
-  removeArtefactFromCollection(artefact: string) {
-    this.artefacts = this.artefacts.filter(item => item != artefact);
-    localStorage.setItem('collection', this.artefacts.join(','));
-    return true;
-  }
-
-  readCollectionFromLocalStorage() {
-    const artefacts = localStorage.getItem('collection');
-    if(artefacts) {
-      this.artefacts = artefacts.split(',');
-    }
+  removeArtefactFromCollection(inventoryNumber: string) {
+    this.artefacts = this.artefacts.filter(item => item.inventoryNumber != inventoryNumber);
+    this.storeCollectionToLocalStorage();
     return true;
   }
 
   showCollection() {
-    this.readCollectionFromLocalStorage();
+    const inventoryNumbers = this.artefacts.map(artefact => artefact.inventoryNumber);
+
     this.rootStore.globalSearch.resetEntityType();
-    const artefactInventoryNumbers = this.artefacts.map(artefact => artefact.replace(/:.*/, ''));
-    this.rootStore.globalSearch.triggerUserCollectionRequest(artefactInventoryNumbers);
+    this.rootStore.globalSearch.triggerUserCollectionRequest(inventoryNumbers);
     return true;
   }
 
+  collectionIncludesArtefact(inventoryNumber: string): boolean {
+    return this.artefacts.some(artefact => artefact.inventoryNumber === inventoryNumber);
+  }
+
   startComparism() {
-    const url = `${cranachCompareURL}${this.artefacts.join(',')}`;
+    const queryParamValue = this.getComparisonQueryParamValue();
+    const url = `${cranachCompareURL}${queryParamValue}`;
     window.open(url, "_blank");
     return true;
+  }
+
+  getComparisonQueryParamValue(): string {
+    return this.artefacts.map((artefact) => {
+      const {
+        inventoryNumber,
+        objectName,
+        entityType,
+      } = artefact;
+
+      const entityTypeCode = entityType.slice(0, 1);
+
+      return `${inventoryNumber}:${objectName}:${entityTypeCode}`;
+    }).join(',');
+  }
+
+  private readCollectionFromLocalStorage() {
+    const artefactsJSON = localStorage.getItem(CRANACH_SEARCH_LOCALSTORAGE_KEY);
+    if(artefactsJSON) {
+      this.artefacts = <CollectionItem[]>JSON.parse(artefactsJSON);
+    }
+    return true;
+  }
+
+  private storeCollectionToLocalStorage(): void {
+    localStorage.setItem(CRANACH_SEARCH_LOCALSTORAGE_KEY, JSON.stringify(this.artefacts));
   }
 }
 
 export interface CollectionStoreInterface {
-  artefacts: string[];
   size: number;
   showCollection(): void;
   startComparism(): void;
-  readFromLocalStorage(): void;
   addArtefactToCollection(artefact: string): void;
   removeArtefactFromCollection(artefact: string): void;
+  collectionIncludesArtefact(id: string): boolean;
 }
