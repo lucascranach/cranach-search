4ff00a4765888bad562c8fc44ee5cd4640419f8c
Jorge H. F. Pereira <jhfpereira@users.noreply.github.com>
2022-04-22 07:08:06 +0200

    fix(sort): remove sorting param and fall back to default api sorting (#67)

--- src/api/globalSearch.ts
+++ src/api/globalSearch.ts
@@ -91,7 +91,6 @@ const getQueryStringForFiltersAndTerm = (
 ): string => {
   const params: Record<string, string | number> = {
     language: langCode,
-    sort_by: 'sorting_number.asc',
     'entity_type:neq': EntityType.DOCUMENTS,
     'size_height:gt': 200, // 9000: 2; 8000: 129; 7000: 393
   };
